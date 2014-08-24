3c2b614c4d8ab36e7153bbfae22e7083cadc0954
Zach Gibb <zachgibb@gmail.com>
2014-08-23 23:02:54 -0400

    Hipchat, in all of it's finest glory

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var hipchat = require('node-hipchat');
 var HC = new hipchat('b9256c4fde0dffc9f84ae1ec8f07fc');
 
-HC.listRooms(function(data) {
-  data.rooms.forEach(function(each) {
-    console.log(each.name);
-  });
-});
+var params = {
+  room: 'Main Room', // Found in the JSON response from the call above
+  message_format: 'text',
+  message: process.argv[2],
+  from: process.argv[3] || 'techno-ghost',
+  color: process.argv[4] || 'random'
+};
 
-HC.listRooms();
+HC.postMessage(params, function(data) {
+  if (data) {
+    console.log('message sent!', data);
+  } else {
+    console.log('message failed to send');
+  }
+});
